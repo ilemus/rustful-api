7654550c704970e0ce27bb93f1fc8e4633604136
ilemus <ilemus@umich.edu>
2022-02-24 11:44:10 -0600

    working on things

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 mod server {
     use std;
     use std::collections::HashSet;
+    use std::collections::HashMap;
 
     pub enum Method {
         GET,
@@ -11,44 +12,58 @@ mod server {
         OPTIONS
     }
     pub struct Request;
-    pub struct Model {
-
+    pub struct Response {
+        pub code: u16
+    }
+    #[derive(Hash)]
+    pub struct Endpoint {
+        pub(crate) method: String,
+        pub(crate) path: String,
     }
     pub struct Api {
-        models: HashSet<Model>
+        pub(crate) endpoints: HashSet<&'static Endpoint>,
+        pub(crate) bindings: HashMap<&'static Endpoint, fn(Request) -> Response>
     }
 
     impl Api {
         pub fn new() -> Api {
-            return Api { models: HashSet::new() };
+            return Api { endpoints: HashSet::new(), bindings : HashMap::new() };
         }
 
-        pub fn add_endpoint(&mut self, path: &str, model: Model) {
-            self.models.insert(model);
-            std::println!("{}", String::from(path));
-            std::println!("{}", model.to_str());
+        pub fn add_endpoint(&mut self, method: String, path: String, handler: fn(Request) -> Response) {
+            let endpoint = Endpoint { method: method, path: path };
+            self.endpoints.insert(&endpoint);
+            self.bindings.insert(&endpoint, handler);
+        }
+
+        pub fn start() {
+
         }
     }
 
-    impl Model {
-        pub(crate) fn to_str(&self) -> String {
-            return String::from("Hello");
-        }
-
-        fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-            // self.action.hash(state);
+    impl PartialEq for Endpoint {
+        fn eq(&self, other: &Self) -> bool {
+            return self.method == other.method
+                && self.path == other.path;
         }
     }
+    impl Eq for Endpoint {}
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    fn api_test_handler(_request: server::Request) -> server::Response {
+        println!("Received request");
+        return server::Response { code: 200, }
+    }
+
     #[test]
-    fn it_works() {
-        let mut x = server::Api::new();
-        let y = server::Model { action: |m, request| println!("received request") };
-        x.add_endpoint("/api", y);
-        assert_eq!(y.to_str(), "Hello");
+    fn test_adding_an_endpoint() {
+        let mut api = server::Api::new();
+        api.add_endpoint(String::from("GET"), String::from("/api/test"), api_test_handler);
+        let my_model = server::Endpoint { method: String::from("GET"), path: String::from("/api/test") };
+        assert!(api.endpoints.contains(&my_model));
     }
 }
