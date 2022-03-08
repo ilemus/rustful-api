fb81219fac13833f6a1bc95b0bc12f6841d7a96f
Isaac Lemus <ilemus@umich.edu>
2022-03-08 17:34:00 -0600

    restructuring and adding start

--- src/lib.rs
+++ src/lib.rs
@@ -22,99 +22,97 @@
 /// ## Api
 /// A collections of endpoints that can be run to start the REST server
  */
-mod rustful_api {
-    use std;
-    use std::collections::HashMap;
-    use std::net::TcpListener;
+use std;
+use std::collections::HashMap;
+use std::net::TcpListener;
 
-    /// # Method
-    /// An enum of allowed operations on endpoints
-    /// - `GET`
-    /// - `PUT`
-    /// - `POST`
-    /// - `DELETE`
-    /// - `OPTIONS`
-    #[derive(Hash, PartialEq)]
-    pub enum Method {
-        GET,
-        PUT,
-        POST,
-        DELETE,
-        OPTIONS
-    }
-    pub struct Request;
-    pub struct Response {
-        pub code: u16
-    }
-    #[derive(Hash)]
-    pub struct Endpoint {
-        pub(crate) method: Method,
-        pub(crate) path: String,
-    }
-    /// # Api
-    /// A collections of endpoints that can be run to start the REST server
-    ///
-    /// ## Example
-    /// ```
-    /// use rustful_api;
-    /// use std;
-    ///
-    /// let mut api = Api::new();
-    /// fn handle_root_request(request: Request) -> Response {
-    ///     println!("Received request on path '/'!");
-    ///     return Response {204};
-    /// }
-    /// api.add_endpoint(Method::GET, String::from("/"), handle_root_request);
-    /// api.start();
-    /// ```
-    pub struct Api {
-        pub(crate) endpoints: HashMap<Endpoint, fn(Request) -> Response>
-    }
-
-    impl Api {
-        pub fn new() -> Api {
-            return Api { endpoints : HashMap::new() };
-        }
-
-        pub fn add_endpoint(&mut self, method: Method, path: String, handler: fn(Request) -> Response) {
-            let endpoint = Endpoint { method, path };
-            self.endpoints.insert(endpoint, handler);
-        }
-
-        pub fn start(&mut self, bind_address: Option<String>, bind_port: Option<u32>) {
-            let hostname = bind_address.unwrap_or(String::from("127.0.0.1"));
-            let port = bind_port.unwrap_or(8080);
-            let tcp_listener = TcpListener::bind(String::from("{}:{}", hostname, port));
-            for stream in tcp_listener.incoming() {
-                let in_stream = stream.unwrap();
-                println!("Connection established!");
-            }
-        }
-    }
-
-    impl PartialEq for Endpoint {
-        fn eq(&self, other: &Self) -> bool {
-            return self.method == other.method
-                && self.path == other.path;
-        }
-    }
-    impl Eq for Endpoint {}
+/// # Method
+/// An enum of allowed operations on endpoints
+/// - `GET`
+/// - `PUT`
+/// - `POST`
+/// - `DELETE`
+/// - `OPTIONS`
+#[derive(Hash, PartialEq)]
+pub enum Method {
+    GET,
+    PUT,
+    POST,
+    DELETE,
+    OPTIONS
 }
+pub struct Request;
+pub struct Response {
+    pub code: u16
+}
+#[derive(Hash)]
+pub struct Endpoint {
+    pub(crate) method: Method,
+    pub(crate) path: String,
+}
+/// # Api
+/// A collections of endpoints that can be run to start the REST server
+///
+/// ## Example
+/// ```
+/// use rustful_api::*;
+///
+/// let mut api = Api::new();
+/// fn handle_root_request(request: Request) -> Response {
+///     println!("Received request on path '/'!");
+///     Response {code: 204}
+/// }
+/// api.add_endpoint(Method::GET, String::from("/"), handle_root_request);
+/// // api.start(None, None);
+/// ```
+pub struct Api {
+    pub(crate) endpoints: HashMap<Endpoint, fn(Request) -> Response>
+}
+
+impl Api {
+    pub fn new() -> Api {
+        Api { endpoints : HashMap::new() }
+    }
+
+    pub fn add_endpoint(&mut self, method: Method, path: String, handler: fn(Request) -> Response) {
+        let endpoint = Endpoint { method, path };
+        self.endpoints.insert(endpoint, handler);
+    }
+
+    pub fn start(&mut self, bind_address: Option<String>, bind_port: Option<u32>) {
+        let hostname = bind_address.unwrap_or(String::from("127.0.0.1"));
+        let port = bind_port.unwrap_or(8080);
+        let tcp_listener = TcpListener::bind(String::from(format!("{}:{}", hostname, port))).unwrap();
+        for stream in tcp_listener.incoming() {
+            let in_stream = stream.unwrap();
+            println!("Connection established!");
+        }
+    }
+}
+
+impl PartialEq for Endpoint {
+    fn eq(&self, other: &Self) -> bool {
+        return self.method == other.method
+            && self.path == other.path;
+    }
+}
+impl Eq for Endpoint {}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    fn api_test_handler(_request: server::Request) -> server::Response {
+    fn api_test_handler(_request: Request) -> Response {
         println!("Received request");
-        return server::Response { code: 200, }
+        Response { code: 200 }
     }
 
     #[test]
     fn test_adding_an_endpoint() {
-        let mut api = server::Api::new();
-        api.add_endpoint(server::Method::GET, String::from("/api/test"), api_test_handler);
-        let my_model = server::Endpoint { method: server::Method::GET, path: String::from("/api/test") };
+        let mut api = Api::new();
+        api.add_endpoint(Method::GET, String::from("/api/test"), api_test_handler);
+        let my_model = Endpoint { method: Method::GET, path: String::from("/api/test") };
         assert!(api.endpoints.contains_key(&my_model));
     }
 }
